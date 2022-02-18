d3b6a6bdc2a442b90be4d0a8ed6410e0ec20be1b
JOY <joybosu@gmail.com>
2022-02-19 00:02:45 +0530

    Chek swap complet or not

---
+++ SwapComplete.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapTwoNumber
+{
+    public class SwapComplete
+    {
+       public static void NumberSwapOrNot()
+        {
+            int a = 5, b = 10;
+            Console.WriteLine("values before Wapping a:{a} and b:{b}");
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("after swaping the value a:{a} and b:{b}");
+        }
+    }
+}
